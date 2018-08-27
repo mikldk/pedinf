b35245f32109a226c655662eaa6c616dd50aeddd
Mikkel Meyer Andersen <mikl@mikl.dk>
2018-08-27 10:16:21 +0200

    update

--- src/pedinf_types.hpp
+++ src/pedinf_types.hpp
@@ -6,9 +6,11 @@
 class Individual;
 class Pedigree;
 class Population;
+//class IdCoefs_Pedigree;
 
 #include "class_Individual.hpp"
 #include "class_Pedigree.hpp"
 #include "class_Population.hpp"
+//#include "idcoefs_pedigree.hpp"
 
 #endif // #ifndef PEDINF_TYPES_H
